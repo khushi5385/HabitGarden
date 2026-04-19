2ba599333cc531dc9735b2919f4af66e807478b7
Khushi Kumari <rair53865@gmail.com>
2026-04-19 12:49:54 +0530

    yes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/HabitGarden/', // MUST MATCH repo name exactly
+  base: '/HabitGarden/', // 🔥 EXACT repo name (case sensitive)
 })
